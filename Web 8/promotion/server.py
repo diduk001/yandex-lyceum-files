6ccc930edd8d5152d2384eb3a829cb326620e057
diduk001 <didurenkos@gmail.com>
2020-03-25 14:57:10 +0500

    Added promotion folder with changed style.css (added <p> tag) file and changed server.py (added promotion page)

---
+++ Web 8/promotion/server.py
@@ -0,0 +1,58 @@
+from flask import Flask, url_for
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def mission_name():
+    return f"""<!doctype html>
+                    <html lang="ru">
+                      <head>
+                        <meta charset="utf-8">
+                        <link rel="stylesheet" type="text/css" href="{url_for('static', filename='css/style.css')}" />
+                        <title>Колонизация Марса</title>
+                      </head>
+                      <body>
+                        <h1>Миссия Колонизация Марса</h1>
+                      </body>
+                    </html>"""
+
+
+@app.route("/index")
+def mission_slogan():
+    return f"""<!doctype html>
+                    <html lang="ru">
+                      <head>
+                        <meta charset="utf-8">
+                        <link rel="stylesheet" type="text/css" href="{url_for('static', filename='css/style.css')}" />
+                        <title>Колонизаця Марса</title>
+                      </head>
+                      <body>
+                        <h1>И на Марсе будут яблони цвести!</h1>
+                      </body>
+                    </html>"""
+
+
+@app.route("/promotion")
+def promotion():
+    return f"""<!doctype html>
+                    <html lang="ru">
+                      <head>
+                        <meta charset="utf-8">
+                        <link rel="stylesheet" type="text/css" href="{url_for('static', filename='css/style.css')}" />
+                        <title>Колонизаця Марса</title>
+                      </head>
+                      <body>
+                        <h1>Рекламная комания</h1>
+                        <br></br>
+                        <p>Человечество вырастает из детства.</p>
+                        <p>Человечеству мала одна планета.</p>
+                        <p>Мы сделаем обитаемыми безжизненные пока планеты.</p>
+                        <p>И начнем с Марса!</p>
+                        <p>Присоединяйся!</p>
+                      </body>
+                    </html>"""
+
+
+if __name__ == '__main__':
+    app.run(port=8080, host='127.0.0.1')
